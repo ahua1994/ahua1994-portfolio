d8b52a3dea216b461ce864847a07985f6e39c17d
Andy Hua <user.andy.hua@gmail.com>
2023-04-03 15:26:20 -0700

    working slider

--- src/pages/Certs.jsx
+++ src/pages/Certs.jsx
@@ -8,12 +8,15 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 const Certs = () => {
     const { certs } = useContext(CVContext);
     const [main, setMain] = useState(0);
-    const [enlarge, setEnlarge] = useState(false);
-
+    const [under, setUnder] = useState(0);
+    // const [enlarge, setEnlarge] = useState(false);
+    console.log(main, under);
     const handleRight = () => {
+        setUnder(main);
         setMain(main < certs.length - 1 ? main + 1 : 0);
     };
     const handleLeft = () => {
+        setUnder(main);
         setMain(main > 0 ? main - 1 : certs.length - 1);
     };
 
@@ -22,14 +25,23 @@ const Certs = () => {
             {/* {certs.map(x => (
                 <CertCard x={x} key={x.name} />
             ))} */}
-            <FontAwesomeIcon size="3x" icon={faChevronLeft} onClick={handleLeft} />
-            <img
-                src={certs[main].img}
-                alt={certs[main].title}
-                style={{ width: enlarge ? "90%" : "1150px" }}
-                onClick={() => setEnlarge(!enlarge)}
+            <FontAwesomeIcon className="left" size="3x" icon={faChevronLeft} onClick={handleLeft} />
+            <div className="carousel">
+                {certs.map((x, i) => (
+                    <div className={i === main ? "slide active" : "slide"} key={i}>
+                        {i === main && <img src={x.img} alt={x.name} />}
+                    </div>
+                ))}
+                <div className="under">
+                    <img src={certs[under].img} alt={certs[under].name} />
+                </div>
+            </div>
+            <FontAwesomeIcon
+                className="right"
+                size="3x"
+                icon={faChevronRight}
+                onClick={handleRight}
             />
-            <FontAwesomeIcon size="3x" icon={faChevronRight} onClick={handleRight} />
         </div>
     );
 };
